dbc5e9e2489da822f584989d877d07d4085d31fb
Andreas <andreas.muttscheller@gmail.com>
2015-12-05 11:55:26 +0100

    Day 5

---
+++ day05/nice.py
@@ -0,0 +1,17 @@
+import re
+
+regexes = [r'(?=(\w*[aeiou]\w*){3,})(?=\w*([a-zA-Z])\2\w*)(?!\w*(ab|cd|pq|xy)\w*)',  # Part 1
+           r'(?=\w*(\w\w)\w*\1\w*)(?=\w*(\w)\w\2\w*)']                               # Part 2
+for i, regex in enumerate(regexes):
+    nice = 0
+    naughty = 0
+    
+    with open('input.txt', 'r') as file:
+        for line in file:
+            r = re.compile(regex)
+            if re.match(r, line):
+                nice += 1
+            else:
+                naughty += 1
+    
+    print('Part {}: {} nice strings, {} naughty strings'.format(i+1, nice, naughty))
