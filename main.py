84c7ed21408d93cbdaef19fde2b99ee61786ea60
Andreas <andreas.muttscheller@gmail.com>
2015-12-05 19:16:57 +0100

    Make main.py more dynamic

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 import os
+import importlib
 
-print('--- Day 1: Not Quite Lisp ---')
+puzzles = [{'t': '--- Day 1: Not Quite Lisp ---', 'm': 'santa'},
+           {'t': '--- Day 2: I Was Told There Would Be No Math ---', 'm': 'wrapping'},
+           {'t': '--- Day 3: Perfectly Spherical Houses in a Vacuum ---', 'm': 'delivery'},
+           {'t': '--- Day 4: The Ideal Stocking Stuffer ---', 'm': 'adventcoins'},
+           {'t': '--- Day 5: Doesn\'t He Have Intern-Elves For This? ---', 'm': 'nice'},
+          ]
+          
+print('Advent of Code 2015\n')
+          
+for i, puzzle in enumerate(puzzles):
+    print(puzzle['t'])
+    day = 'day{:02d}'.format(i+1)
+    os.chdir(day)
+    importlib.import_module(day + '.' + puzzle['m'])
+    os.chdir('..')
+    
+    print()
 
-from day01 import santa
-
-print()
-print('--- Day 2: I Was Told There Would Be No Math ---')
-
-os.chdir('day02')
-from day02 import wrapping
-os.chdir('..')
-
-print()
-print('--- Day 3: Perfectly Spherical Houses in a Vacuum ---')
-
-os.chdir('day03')
-from day03 import delivery
-os.chdir('..')
-
-print()
-print('--- Day 4: The Ideal Stocking Stuffer ---')
-
-os.chdir('day04')
-from day04 import adventcoins
-os.chdir('..')
-
-print()
-print('--- Day 5: Doesn\'t He Have Intern-Elves For This? ---')
-
-os.chdir('day05')
-from day05 import nice
-os.chdir('..')
