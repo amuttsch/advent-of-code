53b48e9146e2e02446e65b4b3b8b148fc41715eb
Andreas <andreas.muttscheller@gmail.com>
2015-12-06 13:25:50 +0100

    Use dicts rather than functions for light and brightness calculation

--- day06/lights.py
+++ day06/lights.py
@@ -5,23 +5,21 @@ regex = re.compile('(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)')
 lights = [[0 for x in range(1000)] for x in range(1000)] # Light grid
 brightness = [[0 for x in range(1000)] for x in range(1000)] # brightness grid
 
-def light(c, v):
-    if c == 'turn on': return 1
-    elif c == 'turn off': return 0
-    elif c == 'toggle': return (v+1)%2
+light = {'turn on': lambda x: 1,
+         'turn off': lambda x: 0,
+         'toggle': lambda x: (x+1)%2}
 
-def bright(c, v):
-    if c == 'turn on': return v+1
-    elif c == 'turn off': return v-1 if v>0 else 0
-    elif c == 'toggle': return v+2
+bright = {'turn on': lambda x: x+1,
+          'turn off': lambda x: x-1 if x>0 else 0,
+          'toggle': lambda x: x+2}
 
 with open('input.txt', 'r') as file:
     for line in file:
         [op, x0, y0, x1, y1] = regex.match(line).groups()
         for x in range(int(x0), int(x1)+1):
             for y in range(int(y0), int(y1)+1):
-                lights[x][y] = light(op, lights[x][y])
-                brightness[x][y] = bright(op, brightness[x][y])
+                lights[x][y] = light[op](lights[x][y])
+                brightness[x][y] = bright[op](brightness[x][y])
 
 active_lights = sum(map(sum, lights))
 total_brightness = sum(map(sum, brightness))
