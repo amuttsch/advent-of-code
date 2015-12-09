a59d6746da35af76fe6e50cf5ef2c01e8840842d
Andreas <andreas.muttscheller@gmail.com>
2015-12-09 13:41:11 +0100

    Shorten code a bit

--- day09/delivery2.py
+++ day09/delivery2.py
@@ -1,7 +1,6 @@
 from itertools import permutations
 import sys
 
-cities = set()
 distances = {}
 
 def read_distances():
@@ -9,13 +8,11 @@ def read_distances():
         (frm, _, to, _, dist) = line.split(' ')
         distances.setdefault(frm, {})[to] = int(dist)
         distances.setdefault(to, {})[frm] = int(dist)
-        cities.add(frm)
-        cities.add(to)
 
 read_distances()
 shortest = sys.maxsize
 longest = 0
-for i in permutations(cities):
+for i in permutations(distances.keys()):
     l = sum(map(lambda x, y: distances[x][y], i[:-1], i[1:]))
     shortest = min(l, shortest)
     longest = max(l, longest)
