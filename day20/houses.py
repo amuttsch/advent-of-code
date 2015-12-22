0bc603488ec115b11296d50a6298eaee3b65b808
Andreas <andreas.muttscheller@gmail.com>
2015-12-22 13:46:18 +0100

    Add day20 part 2

--- day20/houses.py
+++ day20/houses.py
@@ -2,16 +2,25 @@ import math
 from collections import defaultdict
 
 min_number_presents = 34000000
-houses = defaultdict(int)
 
-def part1():
-    for i in range(1, min_number_presents):
-        for j in range(i, min_number_presents // 34, i):
-            houses[j] += i * 10
+def get_house(presents_per_house=10, max_houses=0):
+    houses = defaultdict(int)
+    hits = []
+    for elf in range(1, min_number_presents // 34):
+        m = (max_houses * elf) + 1 if max_houses > 0 else min_number_presents // 34
+        for j in range(elf, m, elf):
+            houses[j] += elf * presents_per_house
             if houses[j] >= min_number_presents:
-                return j
+                hits.append(j)
+                if max_houses == 0:
+                    return j
 
-part_1 = part1()
+    return min(hits)
+
+part_1 = get_house()
+part_2 = get_house(11, 50)
 print('Part 1: house {} gets at first more than {} presents'.format(part_1, min_number_presents))
+print('Part 2: house {} gets at first more than {} presents'.format(part_2, min_number_presents))
 
 assert part_1 == 786240
+assert part_2 == 831600
